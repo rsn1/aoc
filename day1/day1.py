8a606578426cd2418143803b816e579cd323c727
jonathanr <jonisris@hotmail.com>
2022-12-01 14:19:12 +0100

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,24 @@
+def get_total_calories(file):
+    result = []
+    with open(file) as f:
+        data = f.read().splitlines()
+    i = 0
+    sum = 0
+    for j in range(len(data)):
+        if data[j]:
+            sum += int(data[j])
+        else:
+            result.append(sum)
+            sum = 0
+            i += 1
+    return result
+
+
+
+if __name__ == '__main__':
+    tot_cal = get_total_calories('input.txt')
+    sorted_cal = sorted(tot_cal,reverse=True)
+    #task 1
+    print(sorted_cal[0])
+    #task 2
+    print(sum(sorted_cal[0:3]))
